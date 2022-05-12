8ae565870ffefded1da3f32afd6708ea1cd693f4
Ellyzeul <gabrielaug.240@gmail.com>
2022-05-12 17:02:26 -0300

    fix: verificação de existência de uma query string

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ $sep = DIRECTORY_SEPARATOR;
 require_once ".." . $sep . "vendor" . $sep . "autoload.php";
 require_once ".." . $sep . "vendor" . $sep . "pdocrud" . $sep . "pdocrud.php";
 
-use B1system\Route\Router;
+use \B1system\Route\Router;
 
 
 $dotenv = Dotenv\Dotenv::createImmutable("..");
@@ -14,9 +14,11 @@ $requestURI = explode("?", $_SERVER["REQUEST_URI"]);
 $endpoint = $requestURI[0];
 $query_params = [];
 
-foreach(explode("&", $requestURI[1]) as $param) {
-    $pair = explode("=", $param, 2);
-    $query_params = array_merge($query_params, [$pair[0] => $pair[1]]);
+if(isset($requestURI[1])) {
+    foreach(explode("&", $requestURI[1]) as $param) {
+        $pair = explode("=", $param, 2);
+        $query_params = array_merge($query_params, [$pair[0] => $pair[1] ?? null]);
+    }
 }
 
 Router::redirect($uri, $query_params);
