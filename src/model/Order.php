6e66a5fdb3e63d436df22931440e835bd0e1ada7
Gabriel Augusto <gabriel@biblio1.com.br>
2022-05-17 15:08:50 -0300

    feat: exibindo ção dos dados

--- src/model/Order.php
+++ src/model/Order.php
@@ -5,9 +5,19 @@ use \PDOCrud;
 
 class Order
 {
-    public static function read(PDOCrud $crud) {
-        // $crud->joinTable("");
+    public static function read(PDOCrud $crud)
+    {
+        $crud = Order::setFields($crud);
+        $crud = Order::fieldsNotMandatory($crud);
+        $crud = Order::fieldBulkUpdate($crud);
 
+        $response = $crud->dbTable("order_control");
+
+        return $response;
+    }
+
+    private static function setFields(PDOCrud $crud)
+    {
         $crud->crudTableCol([
             "online_order_number",
             "id_sellercentral",
@@ -16,20 +26,51 @@ class Order
             "order_date",
             "expected_date",
             "selling_price",
-            "id_ask_rating"
+            "ask_rating"
         ]);
 
+        $crud->colRename("online_order_number", "ORIGEM");
+        $crud->colRename("id_sellercentral", "Exportação");
+        $crud->colRename("invoice_number", "NF");
+        $crud->colRename("bling_number", "Nº Bling");
+        $crud->colRename("order_date", "Data do pedido");
+        $crud->colRename("expected_date", "Data para entrega");
+        $crud->colRename("selling_price", "Valor");
+        $crud->colRename("ask_rating", "Pedir avaliação");
+        
+        return $crud;
+    }
+
+    private static function fieldsNotMandatory(PDOCrud $crud)
+    {
+        $crud->fieldNotMandatory("invoice_number");
+        $crud->fieldNotMandatory("bling_number");
+        $crud->fieldNotMandatory("ask_rating");
+
+        return $crud;
+    }
+
+    private static function fieldBulkUpdate(PDOCrud $crud)
+    {
+        $crud->bulkCrudUpdate("invoice_number", "text");
+        $crud->bulkCrudUpdate("bling_number", "text");
+        $crud->bulkCrudUpdate("ask_rating", "select", ['askrating_key', 'askrating_val'], [
+            [1, "Sim"],
+            [0, "Não"]
+        ]);
+
+        return $crud;
+    }
+
+    private static function fieldsType(PDOCrud $crud)
+    {
         $crud->fieldTypes("id_sellercentral", "select");
         $query = "SELECT id, name FROM sellercentrals";
         $crud->fieldDataBinding("id_sellercentral", $query, "id", ["id", "name"],"sql", " - ");
 
-        $crud->fieldTypes("invoice_number", "null");
-        $crud->fieldTypes("bling_number", "null");
-
         $crud->fieldTypes("order_date", "date");
+        $crud->fieldTypes("ask_rating", "int");
 
-        $response = $crud->dbTable("orders");
-
-        return $response;
+        return $crud;
     }
 }
