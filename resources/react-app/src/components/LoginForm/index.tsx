f0b5c95bb2b59593d2226c839f5f0711a60d8afe
Gabriel Augusto <gabriel@biblio1.com.br>
2022-07-11 15:23:11 -0300

    it
    feat: login finalizado

--- resources/react-app/src/components/LoginForm/index.tsx
+++ resources/react-app/src/components/LoginForm/index.tsx
@@ -1,13 +1,37 @@
-import { useState } from "react"
+import { FormEventHandler, useRef, useState } from "react"
+import { toast } from "react-toastify"
+import api from "../../services/axios"
+import 'react-toastify/dist/ReactToastify.css'
 import "./style.css"
 
 export const LoginForm = () => {
 	const [passVisible, setPassVisible] = useState(false)
+	const formRef = useRef(null)
 
 	const changePasswordVisibility = () => setPassVisible(!passVisible)
 
+	const makeLogin: FormEventHandler = (event) => {
+		event.preventDefault()
+		if(!formRef.current) return
+		const form = formRef.current as HTMLFormElement
+		const email = (form[0] as HTMLInputElement).value
+		const password = (form[1] as HTMLInputElement).value
+
+		api.post('/api/user/login', {
+			email: email,
+			password: password
+		})
+		.then(response => response.data)
+		.then(response => {
+			const { message, ...userData } = response
+			toast.success(message)
+			window.localStorage.setItem("userData", JSON.stringify(userData))
+			window.location.pathname = '/orders'
+		})
+	}
+
 	return (
-		<form id="login_form">
+		<form id="login_form" ref={formRef} onSubmit={makeLogin}>
 			<h1>Login</h1>
 			<div id="login_inputs">
 				<input type="email" name="login_email" id="login_email" placeholder="E-mail" />
