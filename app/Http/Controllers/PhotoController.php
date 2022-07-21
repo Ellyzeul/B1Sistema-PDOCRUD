c0e5c7e81de40e5e9e8b91771212e8654930e94c
Gabriel Augusto <gabriel@biblio1.com.br>
2022-07-21 15:15:08 -0300

    feat: processamento de fotos a serem salvas

---
+++ app/Http/Controllers/PhotoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Photo;
+
+class PhotoController extends Controller
+{
+    public static function create(string $photoURL, string $photoName) {
+        //
+    }
+}
