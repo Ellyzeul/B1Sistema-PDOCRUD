84814fd09daa6a0d23a0b279b3f888551b2f8d6f
Gabriel Augusto <gabriel@biblio1.com.br>
2022-07-22 17:44:08 -0300

    feat: upload de fotos concluído

--- resources/react-app/src/services/axios.ts
+++ resources/react-app/src/services/axios.ts
@@ -2,11 +2,22 @@ import axios from "axios"
 
 const location = window.location
 const baseURL = location.hostname === "localhost" || location.hostname === "192.168.0.195"
-	? "http://localhost:8000/"
-	: `${location.protocol}//${location.host}/`
+	? "http://localhost:8000"
+	: `${location.protocol}//${location.host}`
 
 const api = axios.create({
 	baseURL: baseURL
 })
+
+export const postFile = (url: string, formData: FormData) => {
+	return axios({
+		method: "POST",
+		url: baseURL + url,
+		data: formData,
+		headers: {
+			"Content-Type": "multipart/form-data"
+		}
+	})
+}
 
 export default api
