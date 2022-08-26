67c920024c946069c9eb8b065157a621d4861384
Gabriel Augusto <gabriel@biblio1.com.br>
2022-08-26 13:00:52 -0300

    fix: correção na escolha de datas

--- resources/react-app/src/components/PDOCrud/functions/configurePage.ts
+++ resources/react-app/src/components/PDOCrud/functions/configurePage.ts
@@ -1,13 +1,14 @@
 import { MutableRefObject } from "react";
 import setValuesOnSelects from "./setValuesOnSelects";
 import setCurrencySymbols from "./setCurrencySymblos";
-import setSearchWorkaround from "./setSearchWorkaround";
+import setConfigurationReapply from "./setConfigurationReapply";
 import setOpenModalEvent from "./setOpenModalEvent";
 import setTopScrollBar from "./setTopScrollBar";
 import setDeadlineColumn from "./setDeadlineColumn";
 import setURLColumn from "./setURLColumn";
 import setPhotoLink from "./setPhotoLink";
 import setSearchTrim from "./setSearchTrim";
+import configureDatepickers from "./configureDatepickers";
 
 const configurePage = (elemRef: MutableRefObject<null>, refModal: MutableRefObject<null>, refModalId: MutableRefObject<null>, refOnlineOrderNumber: MutableRefObject<null>, refURLInput: MutableRefObject<null>) => {
 	if(!elemRef.current) return
@@ -24,9 +25,10 @@ const configurePage = (elemRef: MutableRefObject<null>, refModal: MutableRefObje
 	setCurrencySymbols()
 	setOpenModalEvent(refModal, refModalId, refOnlineOrderNumber, refURLInput)
 	setTopScrollBar(document.querySelector(".panel-body") as HTMLDivElement)
-	setSearchWorkaround(elemRef, refModal, refModalId, refOnlineOrderNumber, refURLInput)
+	setConfigurationReapply(elemRef, refModal, refModalId, refOnlineOrderNumber, refURLInput)
 	setPhotoLink()
 	setSearchTrim()
+	configureDatepickers()
 	if(phase < 7) setDeadlineColumn()
 	if(phase === 2.1) setURLColumn()
 }
