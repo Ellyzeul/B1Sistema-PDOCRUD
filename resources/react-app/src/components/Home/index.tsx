a322a9078adcffaee6684c9b96f22858c31324c3
Gabriel Augusto <gabriel@biblio1.com.br>
2022-09-06 11:35:51 -0300

    refactor: página home convertida em componente

---
+++ resources/react-app/src/components/Home/index.tsx
@@ -0,0 +1,46 @@
+import { useContext, useEffect, useState } from "react"
+import { BookQuotesCard } from "../BookQuotesCard"
+import { UserData } from "../LoginForm/types"
+import { Navbar } from "../Navbar"
+import { SectionButton } from "../SectionButton"
+import { UserInfoCard } from "../UserInfoCard"
+import { UserDataContext } from "../../contexts/UserData"
+import "./style.css"
+import { HomeProp } from "./types"
+
+export const Home = (props: HomeProp) => {
+  const { sections } = props
+  const userDataContext = useContext(UserDataContext)
+  const userData = userDataContext[0] as UserData
+  const [sectionsList, setSectionsList] = useState([] as JSX.Element[])
+
+  useEffect(() => {
+    setSectionsList(sections.map(prop => <SectionButton 
+      label={prop.label} 
+      icon={prop.icon} 
+      url={prop.url} 
+    />))
+  }, [sections])
+
+  return (
+    <div className={"home-page"}>
+      <Navbar items={[]} />
+      <div className={"home-page-content"}>
+        <div className={"home-page-user-info"}>
+          <div>
+            <BookQuotesCard userName={userData.name} />
+          </div>
+          <div>
+            <UserInfoCard userData={userData} />
+          </div>
+        </div>
+        <div className={"home-page-sections"}>
+          {sectionsList}
+        </div>
+        <div className={"home-page-notifications"}>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
