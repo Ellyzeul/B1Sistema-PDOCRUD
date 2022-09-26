0fa03035c929655897099ed8641d4fd7809a75cd
Gabriel Augusto <gabriel@biblio1.com.br>
2022-09-26 12:21:43 -0300

    feat: upload de arquivos para atualização

---
+++ app/Http/Controllers/FileUploadController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\FileUpload;
+
+class FileUploadController extends Controller
+{
+    public function orderUpdate(Request $request)
+    {
+        $fileUpload = new FileUpload();
+        $data = $request->input("upload_data");
+        $response = $fileUpload->orderUpdate($data);
+
+        return $response;
+    }
+}
---
+++ resources/react-app/src/components/UploadFile/constants.ts
@@ -0,0 +1,31 @@
+export const uploadTypes = [
+  {message: "Selecionar...", value: "select"},
+  {message: "Atualização de pedidos", value: "order-update"},
+  // {message: "Envio de pedidos", value: "order-upload"},
+]
+
+export const fields = {
+  "order-update": [
+    {label: "Nº", field_name: "id", updatable: false, required: true},
+    {label: "Empresa", field_name: "id_company", updatable: false, required: false},
+    {label: "Canal de Venda", field_name: "id_sellercentral", updatable: false, required: false},
+    {label: "Fase do processo", field_name: "id_phase", updatable: true, required: false},
+    {label: "NF", field_name: "invoice_number", updatable: true, required: false},
+    {label: "ORIGEM", field_name: "online_order_number", updatable: false, required: true},
+    {label: "Nº Bling", field_name: "bling_number", updatable: true, required: false},
+    {label: "Data do pedido", field_name: "order_date", updatable: false, required: false},
+    {label: "Data prevista", field_name: "expected_date", updatable: false, required: false},
+    {label: "ISBN", field_name: "isbn", updatable: false, required: false},
+    {label: "Valor", field_name: "selling_price", updatable: false, required: false},
+    {label: "Fornecedor", field_name: "supplier_name", updatable: true, required: false},
+    {label: "Data da compra", field_name: "purchase_date", updatable: true, required: false},
+    {label: "Endereço de entrega", field_name: "id_delivery_address", updatable: true, required: false},
+    {label: "Nº Compra fornecedor", field_name: "supplier_purchase_number", updatable: true, required: false},
+    {label: "Forma de envio", field_name: "id_delivery_method", updatable: true, required: false},
+    {label: "Código de rastreio", field_name: "tracking_code", updatable: true, required: false},
+    {label: "Código de coleta", field_name: "collection_code", updatable: true, required: false},
+    {label: "Data de entrega", field_name: "delivered_date", updatable: true, required: false},
+    {label: "Pedir avaliação", field_name: "ask_rating", updatable: true, required: false},
+    {label: "Endereço verificado", field_name: "address_verified", updatable: true, required: false},
+  ]
+} as {[key: string]: {label: string, field_name: string, updatable: boolean, required: boolean}[]}
