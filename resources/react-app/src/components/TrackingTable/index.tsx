4fc4e9870b258bd69d6bb6c604e83da58b749009
Gabriel Augusto <gabriel@biblio1.com.br>
2022-12-03 08:44:28 -0300

    feat: paginação

--- resources/react-app/src/components/TrackingTable/index.tsx
+++ resources/react-app/src/components/TrackingTable/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef, MouseEventHandler, KeyboardEventHandler } from "react"
+import { useEffect, useState, useRef, MouseEventHandler, KeyboardEventHandler, FormEventHandler } from "react"
 import { TrackingTableProp } from "./types"
 import "./style.css"
 import api from "../../services/axios"
@@ -29,10 +29,11 @@ const updateRow = (trackingCode: string, deliveryMethod: string, row: any, field
   console.log(row.children[0].props)
 }
 
-const getRows = (data: {[key: string]: string}[], fieldsKeys: string[]) => {
+const getRows = (data: {[key: string]: string}[], fieldsKeys: string[], actualPage: number) => {
   const rowsElements = [] as JSX.Element[]
+  const offset = actualPage * ROWS_PER_PAGE
 
-  data.forEach((row, idx) => {
+  data.slice(offset, offset + ROWS_PER_PAGE).forEach((row, idx) => {
     const btnCell = <td 
       className="tracking-update-button" 
       onClick={event => updateRow(row.tracking_code, row.delivery_method, rowElement.props, fields)}
@@ -67,12 +68,17 @@ const getFilteredData = (
   return data.filter(row => row[key].startsWith(searchTerm))
 }
 
+const ROWS_PER_PAGE = 5
+
 export const TrackingTable = (props: TrackingTableProp) => {
   const { data } = props as {data: {[key: string]: string}[]}
   const fieldsKeys = Object.keys(fields)
+  const [filteredData, setFilteredData] = useState(data)
   const [filterFields, setFilterFields] = useState([] as JSX.Element[])
+  const [selectOptions, setSelectOptions] = useState([] as JSX.Element[])
   const [headers, setHeaders] = useState(null as JSX.Element | null)
   const [rows, setRows] = useState([] as JSX.Element[])
+  const [actualPage, setActualPage] = useState(0)
   const filterField = useRef(null)
   const filterInput = useRef(null)
 
@@ -80,21 +86,30 @@ export const TrackingTable = (props: TrackingTableProp) => {
     const filtered = getFilteredData(data, filterInput, filterField)
       .sort((a, b) => a[field] > b[field] ? 1 : -1)
 
-    setRows(getRows(filtered, fieldsKeys))
+    setRows(getRows(filtered, fieldsKeys, actualPage))
   }
 
-  const filterData = () => setRows(
-    getRows(getFilteredData(data, filterInput, filterField), fieldsKeys)
+  const filterData = () => setFilteredData(
+    getFilteredData(data, filterInput, filterField)
   )
 
   const filterDataHotkey: KeyboardEventHandler = (event) => event.key === "Enter"
     ? filterData()
     : null
 
+  const changePage: FormEventHandler = (event) => {
+    const select = event.target as HTMLSelectElement
+    const newPage = Number(select.value) - 1
+
+    setActualPage(newPage)
+  }
+
   useEffect(() => {
-    const headerElements = [] as JSX.Element[]
     const optionsElements = [] as JSX.Element[]
-    const rowsElements = getRows(data, fieldsKeys)
+    const selectElements = [] as JSX.Element[]
+    const headerElements = [] as JSX.Element[]
+    const rowsElements = getRows(filteredData, fieldsKeys, actualPage)
+    const totalPages = Math.ceil(filteredData.length / ROWS_PER_PAGE)
 
     fieldsKeys.forEach((key, idx) => {
       headerElements.push(
@@ -106,10 +121,17 @@ export const TrackingTable = (props: TrackingTableProp) => {
       )
     })
 
-    setHeaders(<tr>{[<th></th>, ...headerElements]}</tr>)
+    for(let i = 0; i < totalPages; i++) {
+      selectElements.push(
+        <option key={i}>{i+1}</option>
+      )
+    }
+
     setFilterFields(optionsElements)
+    setSelectOptions(selectElements)
+    setHeaders(<tr>{[<th></th>, ...headerElements]}</tr>)
     setRows(rowsElements)
-  }, [])
+  }, [filteredData, actualPage])
 
   return (
     <div className="tracking-table-container">
@@ -124,7 +146,10 @@ export const TrackingTable = (props: TrackingTableProp) => {
           <select ref={filterField}>{filterFields}</select>
           <button onClick={filterData}>Filtrar</button>
         </div>
-        <select name="" id=""></select>
+        <div>
+          <span>Página </span>
+          <select onChange={changePage}>{selectOptions}</select>
+        </div>
       </div>
       <table className="tracking-table">
         <thead>
