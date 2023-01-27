fad6e2e1e8758b1743884f777170eabf04b92cdf
Gabriel Augusto <gabriel@biblio1.com.br>
2023-01-27 17:53:21 -0300

    refactor: alteração na identação

--- database/migrations/2023_01_27_202410_create_companies_table.php
+++ database/migrations/2023_01_27_202410_create_companies_table.php
@@ -6,32 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('companies', function (Blueprint $table) {
-            $table->tinyIncrements();
-            $table->string('name', 45);
-            $table->tinyText('thumbnail')->nullable();
-            $table->string('company_name', 120)->nullable();
-            $table->tinyText('address')->nullable();
-            $table->string('cnpj', 45)->nullable();
-            $table->string('state_registration', 45)->nullable();
-            $table->string('municipal_registration', 45)->nullable();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('companies', function (Blueprint $table) {
+			$table->tinyIncrements();
+			$table->string('name', 45);
+			$table->tinyText('thumbnail')->nullable();
+			$table->string('company_name', 120)->nullable();
+			$table->tinyText('address')->nullable();
+			$table->string('cnpj', 45)->nullable();
+			$table->string('state_registration', 45)->nullable();
+			$table->string('municipal_registration', 45)->nullable();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('companies');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('companies');
+	}
 };
