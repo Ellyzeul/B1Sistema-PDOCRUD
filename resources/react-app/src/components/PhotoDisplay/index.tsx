f64c8b5f99ee5f5f0a8ebc275a94cdd4c84fffdf
Gabriel Augusto <gabriel@biblio1.com.br>
2023-02-02 08:43:46 -0300

    fix: correção na exclusão de fotos
    O método remove() era utilizado para apagar o card de imagem da tela, implicando num erro de carregamento da página ao tentar executar uma nova busca.
    Sendo assim, foi alterado para o método removeChild() que é mais seguro.

--- resources/react-app/src/components/PhotoDisplay/index.tsx
+++ resources/react-app/src/components/PhotoDisplay/index.tsx
@@ -8,23 +8,46 @@ export const PhotoDisplay = (props: PhotoDisplayProp) => {
   const { url } = props
   const altText = "Imagem não pôde ser carregada. Clique com botão direito e 'Abrir imagem em nova aba' para ver"
   const overlayRef = useRef(null)
+  const trashCanRef = useRef(null)
   const deleteOverlayRef = useRef(null)
   const imgContainerRef = useRef(null)
   const imgRef = useRef(null)
 
-  const onMouseOver: MouseEventHandler = () => {
+  const onMouseEnterOverlay: MouseEventHandler = () => {
     if(!overlayRef.current) return
-    if(!isHidden) return 
+    if(!trashCanRef.current) return
+    if(!isHidden) return
     const overlay = overlayRef.current as HTMLDivElement
+    const trashCan = trashCanRef.current as HTMLDivElement
 
+    trashCan.style.visibility = "visible"
     overlay.style.visibility = "visible"
   }
 
-  const onMouseLeave: MouseEventHandler = () => {
+  const onMouseLeaveOverlay: MouseEventHandler = () => {
     if(!overlayRef.current) return
+    if(!trashCanRef.current) return
     const overlay = overlayRef.current as HTMLDivElement
+    const trashCan = trashCanRef.current as HTMLDivElement
 
+    trashCan.style.visibility = "hidden"
     overlay.style.visibility = "hidden"
+  }
+
+  const onMouseEnterTrashCan = () => {
+    if(!trashCanRef.current) return
+    const trashCan = trashCanRef.current as HTMLDivElement
+
+    trashCan.style.backgroundColor = "white"
+    trashCan.style.color = "red"
+  }
+
+  const onMouseLeaveTrashCan = () => {
+    if(!trashCanRef.current) return
+    const trashCan = trashCanRef.current as HTMLDivElement
+
+    trashCan.style.backgroundColor = "red"
+    trashCan.style.color = "white"
   }
 
   const onClick: MouseEventHandler = () => {
@@ -63,20 +86,26 @@ export const PhotoDisplay = (props: PhotoDisplayProp) => {
     if(!imgContainerRef.current) return
     if(!imgRef.current) return
     const imgContainer = imgContainerRef.current as HTMLDivElement
+    const display = imgContainer.parentElement as HTMLDivElement
     const img = imgRef.current as HTMLImageElement
+    const photoName = img.src.split('/').pop()
 
-    imgContainer.remove()
+    display.removeChild(imgContainer)
 
-    api.delete(`api/photo/exclude?photoName=${img.src.split('/').pop()}`)
-    .then(response => response.data.message)
-    .then(toast.success)
+    api.delete(`api/photo/exclude?photoName=${photoName}`)
+      .then(response => response.data.message)
+      .then(toast.success)
+      .catch(() => {
+        display.appendChild(imgContainer)
+        toast.error("Algum erro ocorreu na exclusão da foto...")
+      })
   }
 
   return (
     <div 
       className="image-display" 
-      onMouseOver={onMouseOver}
-      onMouseLeave={onMouseLeave}
+      onMouseEnter={onMouseEnterOverlay}
+      onMouseLeave={onMouseLeaveOverlay}
       ref={imgContainerRef}
     >
       <img 
@@ -91,8 +120,14 @@ export const PhotoDisplay = (props: PhotoDisplayProp) => {
       >
         Clique para baixar
       </div>
-      <div className="image-overlay-delete">
-        <i className="fa-solid fa-trash" onClick={unhideDelete}></i>
+      <div 
+        className="image-overlay-delete" 
+        ref={trashCanRef} 
+        onClick={unhideDelete} 
+        onMouseEnter={onMouseEnterTrashCan}
+        onMouseLeave={onMouseLeaveTrashCan}
+      >
+        <i className="fa-solid fa-trash"></i>
       </div> 
       <div className="image-overlay-delete-confirm" ref={deleteOverlayRef}>
         <p>Deseja mesmo deletar essa foto?</p>
