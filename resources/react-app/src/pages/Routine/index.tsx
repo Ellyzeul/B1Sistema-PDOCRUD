956a0d479da5c59288b3bd3809921788bcc899e5
yingyangtongxue <danielnm60@gmail.com>
2023-06-12 09:21:51 -0300

    fix: caminho componente tabManager

--- resources/react-app/src/pages/Routine/index.tsx
+++ resources/react-app/src/pages/Routine/index.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "../../components/Navbar"
 import { tabContentAlibrisSelineUSA, tabContentAmazonB1BR, tabContentAmazonB1USA, tabContentAmazonSelineBR, 
 tabContentAmazonSelineUSA, tabContentEmails, tabContentEstanteVirtual, tabContentFNACptSeline, tabContentGeneral } from "./tabContents"
 import "./style.css"
-import { TabManager } from "../../components/TabManager"
+import { TabManager } from "../../components/tabManager"
 
 export const RoutinePage = () => {
   const contents = {
