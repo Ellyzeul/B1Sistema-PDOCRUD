13c141dd4992e9b902f99191eb232273c840fcc9
Gabriel Augusto <gabriel@biblio1.com.br>
2024-01-29 14:46:23 -0300

    fix: cotação de transportadoras

--- resources/react-app/src/components/PDOCrud/components/AddressModal/AddressForm/ShipmentAndPrice/types.ts
+++ resources/react-app/src/components/PDOCrud/components/AddressModal/AddressForm/ShipmentAndPrice/types.ts
@@ -7,20 +7,18 @@ export type ShipmentAndPriceProp = {
 	id_bling: number,
 } 
 
-export type CorreiosData = {
-	[key: string]: {
-		service_name: string,
-		delivery_expected_date: number | null,
-		max_date: string | null,
-		shipping_error_msg: string | null,
-		price: string | null,
-		price_error_msg: string | null,
-	}
-}
+export type CorreiosData = Array<{
+	name: string,
+	expected_deadline: number | null,
+	expected_date: string | null,
+	shipping_error_msg: string | null,
+	price: string | null,
+	price_error_msg: string | null,
+}>
 
 export type JadlogData = {
 	price: number | null,
-	max_date: number | null,
+	expected_deadline: number | null,
 	error_msg: string | null
 }
 
