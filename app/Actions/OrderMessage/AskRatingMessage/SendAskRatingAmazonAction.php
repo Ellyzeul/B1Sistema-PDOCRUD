129db0421356957eddde1548dfbfee69efb6ffb0
Gabriel Augusto <gabrielaug.240@gmail.com>
2024-02-27 12:09:33 -0300

    fix: envio de mensagem de avaliação na Amazon

--- app/Actions/OrderMessage/AskRatingMessage/SendAskRatingAmazonAction.php
+++ app/Actions/OrderMessage/AskRatingMessage/SendAskRatingAmazonAction.php
@@ -1,12 +1,20 @@
 <?php namespace App\Actions\OrderMessage\AskRatingMessage;
 
 use App\Actions\OrderMessage\Traits\AskRatingMessageCommon;
+use App\Models\Order;
 use Illuminate\Support\Facades\DB;
-use App\Mail\AskRatingAmazon;
+use Illuminate\Support\Facades\Http;
 
 class SendAskRatingAmazonAction
 {
     use AskRatingMessageCommon;
+
+    private string $sellercentral;
+
+    public function __construct(string $sellercentral)
+    {
+        $this->sellercentral = $sellercentral;
+    }
 
     public function handle(string $orderId)
     {
@@ -15,29 +23,33 @@ class SendAskRatingAmazonAction
 
     private function sendAskRatingEmail(string $orderId)
     {
-        [ $blingNumber, $apikey, $fromEmail, $companyName, $isNational ] = $this->getMailingInfo($orderId);
+        $order = Order::where('id', $orderId)->first();
+        $address = DB::table('order_addresses')
+            ->where('online_order_number', $order->online_order_number)
+            ->first();
+    
+        if(!isset($order) || !isset($address)) return [
+            'success' => false,
+            'content' => $this->errorMessage($order, $address, $orderId),
+        ];
 
-        $blingResponse = $this->getBlingMessagingInfo($apikey, $blingNumber);
-        if(isset($blingResponse['error'])) return $blingResponse['error'];
-
-        [ $clientName, $clientEmail, $orderNumber, $bookName ] = $blingResponse;
-
-        $content = new AskRatingAmazon(
-            $fromEmail, 
-            $isNational,
-            $clientName, 
-            $orderNumber, 
-            $bookName, 
-            $companyName, 
-        );
-
-        $this->sendEmail($clientEmail, $content);
+        $response = Http::b1servicos()->post('/message/ask-rating', [
+            'order_number' => $order->online_order_number,
+            'client_name' => $address->buyer_name,
+            'client_email' => $address->buyer_email,
+            'sellercentral' => $this->sellercentral,
+        ]);
         
-        DB::table('order_control')
-            ->where('id', $orderId)
-            ->increment('ask_rating');
+        Order::where('id', $orderId)->increment('ask_rating');
 
-        return [["message" => "E-mail enviado com sucesso!"], 200];
+        return $response->success
+            ? [["message" => "E-mail enviado com sucesso!"], 200]
+            : [["message" => $response->errPayload], 400];
     }
-   
+
+    private function errorMessage(?object $order, ?object $address, string $id)
+    {
+        if(!isset($order)) return "Pedido de ID $id inexistente no sistema...";
+        if(!isset($address)) return "Endereço do pedido $order->online_order_number inexistente no sistema...";
+    }
 }
