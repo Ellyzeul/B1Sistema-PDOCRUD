d9c31fa02e9598b53f46f4817feb07eaae9971b1
Gabriel Augusto <gabrielaug.240@gmail.com>
2024-06-28 16:17:44 -0300

    feat: envio de atualização de rastreio interno

--- resources/react-app/src/components/PDOCrud/functions/setSaveEvent.ts
+++ resources/react-app/src/components/PDOCrud/functions/setSaveEvent.ts
@@ -6,13 +6,16 @@ export default function setSaveEvent() {
   const saveBtn = document.querySelector('.pdocrud-button-save') as HTMLAnchorElement
   const rows = getTableRows()
   const onlineOrderNumberIndex = getColumnFieldIndex('ORIGEM')
+  const phaseIndex = getColumnFieldIndex('Fase do processo')
 
   saveBtn.addEventListener('click', async(event) => {
     event.preventDefault()
-    const orderNumbers = rows
-      .map(({children: row}) => row[onlineOrderNumberIndex] as HTMLTableCellElement)
-      .map(cell => cell.innerText)
-    
-    api.post('/api/tracking/update-internal', {order_numbers: orderNumbers})
+    const ordersPhases = {} as Record<string, string>
+    rows.map(({children: row}) => [row[onlineOrderNumberIndex], row[phaseIndex]] as [HTMLTableCellElement, HTMLTableCellElement])
+      .forEach(([numberCell, phaseCell]) => 
+        ordersPhases[numberCell.innerText] = (phaseCell.children[0] as HTMLSelectElement).value
+      )
+
+    api.post('/api/tracking/update-internal', {orders_phases: ordersPhases})
   })
 }
